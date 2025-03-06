f0ca62f9170aa886ba795746bb32e06c3d819fa0
Katie Te Nahu Owen <katie@deliciousmint.net>
2025-03-06 23:04:26 +1300

    Fix hover state for contact form button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        button: "bg-transparent hover:bg-primary/90",
+        button: "bg-transparent",
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
