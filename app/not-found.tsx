c18746595448ee15c07cca128d6ac6ec72bcf27e
Katie Te Nahu Owen <katie@deliciousmint.net>
2025-03-14 12:06:37 +1300

    404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,19 +6,15 @@ import React from "react";
 
 export default function NotFound() {
   return (
-      <Section className="page-page">
+      <Section className="page-page page-not-found">
         <Container>
-          <div
-              className="mx-90px page-header"
-          >
-            <h1 className="h3-headings-and-pullquotes md:h2-headings-and-intros">404 - Page Not Found</h1>
-          </div>
           <div className="mx-90px grid max-md:grid-cols-4 md:grid-cols-16 md:gap-6">
             <div className="page-html">
-              <p className="pb-8">
-                Sorry, the page you are looking for does not exist.
+              <h1 className="h1-article-headings">Page not found</h1>
+              <p>
+                Sorry, this page isn’t available anymore or an error occurred.
               </p>
-              <Link href="/" className="button border py-3 px-10 border-black bg-transparent">Back to Home</Link>
+              <Link href="/" className="button border py-3 px-10 border-black bg-transparent">Go back</Link>
             </div>
           </div>
         </Container>
